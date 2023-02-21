f6eeeb3bacacf1763b5934b081d0f19dbca30f23
Daria Tkachenko <113775461+dariaaferreira@users.noreply.github.com>
2023-02-21 18:37:05 +0200

    start

---
+++ src/js/task-02.js
@@ -0,0 +1,19 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+
+const items = ingredients.map(ingredient => {
+  const itemEl = document.createElement("li");
+  itemEl.classList.add("item");
+  itemEl.textContent = ingredient;
+  // console.log(itemEl);
+  return itemEl;
+});
+
+const ulEl = document.querySelector('#ingredients');
+ulEl.append(...items);
