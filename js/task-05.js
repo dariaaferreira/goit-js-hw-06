3c5d2226e7f572d21b53a7d5b9e0ab674c26b74f
Daria Tkachenko <113775461+dariaaferreira@users.noreply.github.com>
2023-02-22 16:21:05 +0200

    done

---
+++ js/task-05.js
@@ -0,0 +1,10 @@
+const inputName = document.querySelector('#name-input');
+const outputName = document.querySelector('#name-output');
+
+inputName.addEventListener('input', (event) => 
+outputName.textContent = event.currentTarget.value === "" 
+? "Anonymous" 
+: event.currentTarget.value
+);
+
+
